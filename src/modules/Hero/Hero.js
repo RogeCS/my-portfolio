f2aefe77225ec0e0a80e6bd33539c8e6a1a4b171
Rogelio Medina <rogelio.medina.cs@gmail.com>
2021-10-06 11:39:23 -0500

    removed my photo

--- src/modules/Hero/Hero.js
+++ src/modules/Hero/Hero.js
@@ -11,14 +11,9 @@ const Hero = () => {
 		<div>
 			<MaxWidthContainer>
 				<section className={styles.hero__container}>
-					<div className={styles.myImage__wrapper}>
-						<div className={styles.myImage__wrap}>
-							<Image src="/assets/static/my-photo.jpg" alt="rogelio's photo" height="200" width="200" layout="responsive" />
-						</div>
-					</div>
 					<hgroup className={styles.texts__wrapper}>
-						<h1 className={styles.name}>Rogelio Medina</h1>
-						<h2 className={styles.job}>Frontend Developer</h2>
+						<h1 className={styles.name}>Hi, I'm<br/> Rogelio</h1>
+						<p className={styles.profile_description}>Welcome to my portfolio. A happy frontend developer</p>
 						<CircleFillBounce scrollTo="about-me">
 							<BsArrowDownShort />
 						</CircleFillBounce>
