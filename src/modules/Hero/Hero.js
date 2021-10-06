4922d059d6039d4ac76812eabf092a9a6826dcc5
Rogelio Medina <rogelio.medina.cs@gmail.com>
2021-10-06 11:45:37 -0500

    removed description at hero

--- src/modules/Hero/Hero.js
+++ src/modules/Hero/Hero.js
@@ -1,6 +1,4 @@
-import Image from 'next/image';
 import { BsArrowDownShort } from 'react-icons/bs';
-import FullScreenContainer from '@containers/FullScreenContainer';
 import MaxWidthContainer from '@containers/MaxWidthContainer';
 import CircleFillBounce from '@animations/CircleFillBounce';
 
@@ -13,7 +11,6 @@ const Hero = () => {
 				<section className={styles.hero__container}>
 					<hgroup className={styles.texts__wrapper}>
 						<h1 className={styles.name}>Hi, I'm<br/> Rogelio</h1>
-						<p className={styles.profile_description}>Welcome to my portfolio. A happy frontend developer</p>
 						<CircleFillBounce scrollTo="about-me">
 							<BsArrowDownShort />
 						</CircleFillBounce>
